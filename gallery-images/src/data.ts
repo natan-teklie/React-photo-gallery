2bf1102ddff4cac5aac619d8a5f0e22a81ed9fbf
natan-teklie <natanmitiku232@gmail.com>
2024-04-13 15:52:16 +0300

    updated

---
+++ gallery-images/src/data.ts
@@ -0,0 +1,15 @@
+export const slides =[
+{src:'https://images.unsplash.com/photo-1580927752452-89d86da3fa0a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title:'slide title ',
+    description:'slide description one ',
+},
+{src:'https://images.unsplash.com/photo-1536148935331-408321065b18?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+title:'slide ',
+description:'slide description one '},
+{src:'https://images.unsplash.com/photo-1605379399843-5870eea9b74e?q=80&w=2098&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+title:'slid',
+description:'slide description one '},
+{src:'https://images.unsplash.com/photo-1555099962-4199c345e5dd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+title:'slide title one',
+description:'slide description on '}
+]
